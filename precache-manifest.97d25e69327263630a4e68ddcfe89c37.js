d8e0b6bea28a5a781f020e169db7d2a8f292fa14
nataliesun <natsun121@gmail.com>
2019-05-25 15:47:39 -0700

    Updates

---
+++ precache-manifest.97d25e69327263630a4e68ddcfe89c37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b26665397d1f506c455",
+    "url": "/portfolio/static/css/main.4947f119.chunk.css"
+  },
+  {
+    "revision": "0b26665397d1f506c455",
+    "url": "/portfolio/static/js/main.657fc4bd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "70f872b49cc9f086025b",
+    "url": "/portfolio/static/js/2.b92acc25.chunk.js"
+  },
+  {
+    "revision": "cb4a0c5277ed056aeeda10f37ac32c99",
+    "url": "/portfolio/static/media/Logo.cb4a0c52.png"
+  },
+  {
+    "revision": "390ccb6baff0b7616edf8c787db2d0ea",
+    "url": "/portfolio/static/media/nataliesun.390ccb6b.jpg"
+  },
+  {
+    "revision": "b6e5483fd116ee58fa28bd72535811f4",
+    "url": "/portfolio/static/media/forDrizzle.b6e5483f.jpg"
+  },
+  {
+    "revision": "4ba9d4210010a857c4becab2730f031c",
+    "url": "/portfolio/static/media/quizApp.4ba9d421.jpg"
+  },
+  {
+    "revision": "4f59468257bf2049a9c73af24301b4fc",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47dabb3fede7c3160087f12594126f8d.js"
+  "/portfolio/precache-manifest.97d25e69327263630a4e68ddcfe89c37.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
