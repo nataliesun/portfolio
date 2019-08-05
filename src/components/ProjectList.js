0034778515f827264dfde80e90ad921833a7167d
nataliesun <natsun121@gmail.com>
2019-08-05 12:43:21 -0700

    changed estatecloud url

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -15,7 +15,7 @@ class ProjectList extends Component {
 
         return (
             <main className="ProjectList">
-                <Project image={estateCloud} title='EstateCloud' description="React, responsive design, HTML5, CSS3, Node.js, Express, Postgresql" link="https://estatecloud-app.nataliesun.now.sh" repo="https://github.com/nataliesun/for-drizzle" />
+                <Project image={estateCloud} title='EstateCloud' description="React, responsive design, HTML5, CSS3, Node.js, Express, Postgresql" link="https://estatecloud.nataliesun.now.sh" repo="https://github.com/nataliesun/for-drizzle" />
 
                 <Project image={forDrizzle} title='For Drizzle' description="Javascript, jQuery, HTML5, CSS3, RestfulAPI" link="https://nataliesun.github.io/for-drizzle/" repo="https://github.com/nataliesun/estatecloud-app" />
 
